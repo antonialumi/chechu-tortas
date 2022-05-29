87ffb93f501f56491b62bf20dd21c76baa263fc2
leo schvartzman <martin.schvartzman@gmail.com>
2022-05-29 20:49:51 -0300

    v0.0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <>hola</>
-  )
+  return (<>
+    <h1>Proximamente...</h1>
+    <p>
+      la pagina web super wachiwaw de Chechu la ,ejor baker chance in the farafan worrrlddd LOL #YOLO
+    </p>
+  </>)
 }
 
 export default Home
